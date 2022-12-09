7de093f6e4f1b497e5e6b57752bc6e6e1eeeff6c
SifaShaikh <114479739+SifaShaikh@users.noreply.github.com>
2022-12-09 09:04:18 +0000

    Added a for loop for the frequencies function

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,8 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
     # Your code goes here
+    frequencies = {}
+    for item in items:
+        frequencies[str(item)] = frequencies.get(str(item), 0)+1
     return frequencies
